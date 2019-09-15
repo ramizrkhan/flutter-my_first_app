38713d730369959c1a6110e3f1f4e39684adb7a3
ramizrkhan <ramizkhan@localhost.localdomain>
2019-09-16 01:43:44 +0530

    Added widgets and Function

--- README.md
+++ README.md
@@ -5,8 +5,11 @@ A new Flutter project.
 Started Learning new Tech ,
 this repo created for my practical tracking purpuse. day by day will I will add my work commit in this file... 
 # 14/09/2019
-Created First Project & reposatoray and removed default code . 
-Created First Text Widget and app from scratch.
+Created First Project & reposatoray and removed default code . <br />
+Created First Text Widget and app from scratch.<br />
 # 15/09/2019
-1 ] Shorthand for one expression in one line. 
-
+1 ] Shorthand for one expression in one line. <br />
+# 16/09/2019
+1] Aligned app widgets in Scaffold widget.<br />
+2] Added Column ,RaisedButton, Text Widget .<br />
+3] Added function for RaisedButton.<br />
--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,44 @@ import 'package:flutter/material.dart';
 // void main() {
 // runApp(MyApp());
 // }
-void main () => runApp(MyApp()); //Shorthand for one expression in one line  
+void main() => runApp(MyApp()); //Shorthand for one expression in one line
+
 class MyApp extends StatelessWidget {
+  void answerQuestion(){
+    print("Answer chosen !");
+  }
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text('Ramiz'),);
+     var questions = [
+    "What\'s your favorite color ?",
+    "What\'s your favorite animal ?"
+  ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Title"),
+        ),
+        body: Column(
+          children: [
+            Text('TheQuestions !'),
+            RaisedButton(
+              child: Text(
+                "Answer 1 ",
+              ),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text("Answer 2"),
+              onPressed: ()=>print("Answer 2 choosen!"),
+            ),
+            RaisedButton(
+              child: Text("Answer 3"),
+              onPressed: (){
+                print("Answer 3 chosen !");
+              },
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
