d3b63b1b55037556174e745cbf529d9280f73d2e
ramizrkhan <ramizkhan@localhost.localdomain>
2019-09-17 01:39:52 +0530

    Changed Statelesswidget to statefulwidgit for changing question on clicking button

--- lib/main.dart
+++ lib/main.dart
@@ -5,15 +5,29 @@ import 'package:flutter/material.dart';
 // }
 void main() => runApp(MyApp()); //Shorthand for one expression in one line
 
-class MyApp extends StatelessWidget {
-  void answerQuestion(){
-    print("Answer chosen !");
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MyAppState();
   }
+}
+
+class MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+  void answerQuestion() {
+    setState(() {
+      questionIndex = questionIndex + 1;
+    });
+    print("Answer chosen !");
+    print(questionIndex);
+  }
+
   Widget build(BuildContext context) {
-     var questions = [
-    "What\'s your favorite color ?",
-    "What\'s your favorite animal ?"
-  ];
+    var questions = [
+      "What\'s your favorite color ?",
+      "What\'s your favorite animal ?"
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -21,7 +35,7 @@ class MyApp extends StatelessWidget {
         ),
         body: Column(
           children: [
-            Text('TheQuestions !'),
+            Text(questions[questionIndex]),
             RaisedButton(
               child: Text(
                 "Answer 1 ",
@@ -30,11 +44,11 @@ class MyApp extends StatelessWidget {
             ),
             RaisedButton(
               child: Text("Answer 2"),
-              onPressed: ()=>print("Answer 2 choosen!"),
+              onPressed: () => print("Answer 2 choosen!"),
             ),
             RaisedButton(
               child: Text("Answer 3"),
-              onPressed: (){
+              onPressed: () {
                 print("Answer 3 chosen !");
               },
             ),
